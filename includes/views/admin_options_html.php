01f2472a6f626a0a802794ee5ffc5907b82acf2f
IceNox GmbH <info@icenox.com>
2025-01-10 18:52:49 +0100

    Update Edit Payment Method View

--- includes/views/admin_options_html.php
+++ includes/views/admin_options_html.php
@@ -1,4 +1,4 @@
-<h3><?php echo __( 'IceNox Pay Payment Settings', 'woocommerce-icenox-pay-plugin' ); ?></h3>
+<h3><?php echo __( "Payment Method Settings", "woocommerce-icenox-pay-plugin" ) . " - " . $this->method_title; ?></h3>
 <div id="poststuff">
     <div id="post-body" class="metabox-holder columns-2">
         <div id="post-body-content">
@@ -9,7 +9,7 @@
         <div id="postbox-container-1" class="postbox-container">
             <div id="side-sortables" class="meta-box-sortables ui-sortable">
                 <div class="postbox " id="icenox-support">
-                    <h3><?php echo __( 'Need Help?', 'woocommerce-icenox-pay-plugin' ); ?></h3>
+                    <h3><?php echo __( "Need Help?", "woocommerce-icenox-pay-plugin" ); ?></h3>
                     <div class="inside">
                         <div class="support-widget">
                             <p>
@@ -18,18 +18,15 @@
                                 <br/>
                                 <br/>
                                 <br/>
-								<?php echo __( 'Contact our Merchant Support, if you need help configuring the plugin.', 'woocommerce-icenox-pay-plugin' ); ?>
+								<?php echo __( "Contact our Merchant Support, if you need help configuring the plugin.", "woocommerce-icenox-pay-plugin" ); ?>
                             </p>
                             <ul>
-                                <li>» <a href="mailto:info@icenox.com"
-                                         target="_blank"><?php echo __( 'Email Us', 'woocommerce-icenox-pay-plugin' ); ?></a>
-                                </li>
+                                <li>» <a href="mailto:info@icenox.com" target="_blank"><?php echo __( "Email Us", "woocommerce-icenox-pay-plugin" ); ?></a></li>
                             </ul>
 
                         </div>
                     </div>
                 </div>
-
             </div>
         </div>
     </div>
